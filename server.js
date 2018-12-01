0371b9c8cfea0e5789f31378f24869c596c1178d
Ronald Munoz <ronald1195@icloud.com>
2018-11-30 17:12:47 -0700

    Finished chat

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
+    usernames = [],
     path = require('path');
 
     //to include external files into our code...
@@ -18,15 +19,43 @@ var express = require('express'),
     res.sendFile(__dirname + '/index.html');
     });
 
-
-
     //connecting to socket.
     io.sockets.on('connection', function(socket){
         console.log('Socket connected...');
 
         socket.on('send message', function(data){
-            io.sockets.emit('new message', {msg:data});
+            io.sockets.emit('new message', {msg:data, user:socket.username});
+    });
+
+        //creating a new user
+        socket.on('new user', function(data, callback){
+            console.log('getting new user');
+
+            if(usernames.indexOf(data) != -1){
+                callback(false);
+            }else{
+                callback(true);
+                socket.username = data;
+                usernames.push(socket.username);
+                updateUsernames();
+            }
+
         });
+
+        //update usernames functions
+        function updateUsernames(){
+            io.sockets.emit('usernames', usernames);
+        }
+
+
+        //disconnect
+        socket.on('disconnect', function(data){
+        if(!socket.username){
+            return;
+        }
+
+        usernames.splices(usernames.indexOf(socket.username), 1);
+    });
 
     });
 
